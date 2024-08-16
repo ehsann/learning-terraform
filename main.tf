f8e6394d1746ac1dcd6af4b354c860ada68155fb
Ehsan Khorram <e.khorram@gmail.com>
2024-08-16 16:34:12 -0400

    autosclaing

--- main.tf
+++ main.tf
@@ -34,18 +34,19 @@ module "blog_vpc" {
   }
 }
 
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.0.0"
+  # insert the 1 required variable here
+  name = "blog"
+  min_size = 1
+  max_size = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_groups_arns = module.blog_alb.target_groups_arns
+  security_groups = [module.blog_sg.security_group_id] 
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id] 
-  
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
 
 }
 

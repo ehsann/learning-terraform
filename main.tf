01c1fe6f6a523c3aa478d899d2060ce3fd35d8a0
Ehsan Khorram <e.khorram@gmail.com>
2024-08-16 13:51:21 -0400

    load balancer

--- main.tf
+++ main.tf
@@ -49,6 +49,30 @@ resource "aws_instance" "blog" {
 
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  
+  
+  http_tcp_listeners = [
+    {
+      port = 80
+      protocol = 'http'
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
